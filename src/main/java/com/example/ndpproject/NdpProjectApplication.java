4e30abe717a209ae4ca1ca523fbb3fabd534319d
Fatih Yıldız <charvisfk@gmail.com>
2025-10-22 13:56:23 +0300

    Initial Commit

---
+++ src/main/java/com/example/ndpproject/NdpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ndpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NdpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ndpproject/NdpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ndpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
